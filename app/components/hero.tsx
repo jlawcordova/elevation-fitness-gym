3585095c6497484d9e8912ff3a1b0c33a8364472
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-09-07 19:19:11 +0800

    feat: add marketing background video

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,16 @@
 export default function Hero() {
   return (
-    <div className="hero min-h-[calc(100vh-4rem)] bg-base-200">
+    <div className="hero min-h-[calc(100vh-4rem)] bg-base-200 bg-opacity-80">
+      <div className="relative w-full min-h-[calc(100vh-4rem)] -z-10 overflow-hidden">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          loop
+          autoPlay
+          muted
+        >
+          <source src="/elevation-video.mov"></source>
+        </video>
+      </div>
       <div className="hero-content text-center">
         <div className="max-w-lg">
           <h1 className="text-6xl md:text-8xl font-bold">
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Footer from "./components/footer";
 import Hero from "./components/hero";
 
 export default function Home() {
-  return <Hero></Hero>;
+  return (
+    <>
+      <Hero></Hero>
+      <Footer></Footer>
+    </>
+  );
 }
