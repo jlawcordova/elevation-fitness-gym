02c34e7a997f36ca238a97998034d15caeb5c1e7
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-09-08 23:56:28 +0800

    chore: use grayscale background video

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <div className="hero min-h-[calc(100vh-4rem)] bg-base-200 bg-opacity-80">
       <div className="relative w-full min-h-[calc(100vh-4rem)] -z-10 overflow-hidden">
         <video
-          className="absolute top-0 left-0 w-full h-full object-cover"
+          className="absolute top-0 left-0 w-full h-full object-cover grayscale"
           loop
           autoPlay
           muted
@@ -12,10 +12,15 @@ export default function Hero() {
         </video>
       </div>
       <div className="hero-content text-center">
-        <div className="max-w-lg">
-          <h1 className="text-6xl md:text-8xl font-bold">
-            <span className="text-primary">Elevate</span> your potential, level
-            up your <span className="text-primary">fitness</span>
+        <div className="max-w-3xl">
+          <h1 className="mb-16 text-6xl md:text-8xl uppercase font-bold drop-shadow-2xl">
+            <span className="text-primary">Elevate</span>
+            <br />
+            your potential.
+            <br />
+            level up your
+            <br />
+            <span className="text-primary">fitness.</span>
           </h1>
         </div>
       </div>
