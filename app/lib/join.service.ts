ab6bf6417bd23ef2abc29540bd5b9901d63a002b
J. Law. Cordova <hello@jlawcordova.com>
2025-09-10 22:08:29 +0800

    Move Supabase join request logic to service

--- app/lib/join.service.ts
+++ app/lib/join.service.ts
@@ -7,14 +7,24 @@ export interface JoinCommand {
 }
 
 export const Join = async (joinCommand: JoinCommand): Promise<boolean> => {
-  // Simulate a 2 second save.
-  const promise = new Promise<boolean>((resolve, reject) => {
-    setTimeout(() => {
-      resolve(true);
-    }, 2000);
+  const url = `${process.env.SUPABASE_URL!}/rest/v1/join_requests`;
+  const apiKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: apiKey,
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      gym_id: joinCommand.gymId,
+      gym_plan_id: joinCommand.gymPlanId,
+      first_name: joinCommand.firstName,
+      last_name: joinCommand.lastName,
+      email: joinCommand.email,
+    }),
   });
 
-  const gym = await promise;
-
-  return gym;
+  return response.ok;
 };
