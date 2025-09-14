af2191db92fad274120704f99fd8e4d3b770afe7
J. Law. Cordova <hello@jlawcordova.com>
2025-09-14 18:21:47 +0800

    feat: add mobile list/map toggle

--- app/gyms/components/gym-search.tsx
+++ app/gyms/components/gym-search.tsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 import { GymList } from "./gym-list";
 import Gym from "../../lib/gym.interface";
 import GymMap from "./gym-map";
+import { MapIcon, ListBulletIcon } from "@heroicons/react/24/solid";
 
 export default function GymSearch({ gyms }: { gyms: Gym[] }) {
   const [selectedGym, setSelectedGym] = useState<Gym | undefined>(
     gyms.length > 0 ? gyms[0] : undefined,
   );
+  const [showMap, setShowMap] = useState(false);
 
   function handleGymSelect(gym: Gym) {
     setSelectedGym(gym);
@@ -22,15 +24,43 @@ export default function GymSearch({ gyms }: { gyms: Gym[] }) {
           Time to elevate your fitness.
         </h1>
         <p>Choose a gym near you to view available membership options.</p>
+        <div className="flex justify-end gap-2 mt-4 lg:hidden">
+          <button
+            aria-label="List view"
+            className={`btn btn-sm btn-square rounded-none ${
+              showMap ? "btn-ghost" : "btn-primary"
+            }`}
+            onClick={() => setShowMap(false)}
+          >
+            <ListBulletIcon className="w-5 h-5" />
+          </button>
+          <button
+            aria-label="Map view"
+            className={`btn btn-sm btn-square rounded-none ${
+              showMap ? "btn-primary" : "btn-ghost"
+            }`}
+            onClick={() => setShowMap(true)}
+          >
+            <MapIcon className="w-5 h-5" />
+          </button>
+        </div>
       </div>
-      <div className="z-10 overflow-auto lg:max-h-[calc(100vh-4rem)] lg:row-start-1 lg:col-start-1 lg:w-[24rem]">
+      <div
+        className={`z-10 overflow-auto lg:max-h-[calc(100vh-4rem)] lg:row-start-1 lg:col-start-1 lg:w-[24rem] ${
+          showMap ? "hidden lg:block" : ""
+        }`}
+      >
         <GymList
           gyms={gyms}
           selectedGym={selectedGym}
           onGymSelect={handleGymSelect}
         />
       </div>
-      <div className="hidden lg:block lg:row-start-1 lg:col-start-1 lg:col-span-12">
+      <div
+        className={`${
+          showMap ? "" : "hidden lg:block"
+        } lg:row-start-1 lg:col-start-1 lg:col-span-12`}
+      >
         <GymMap
           gyms={gyms}
           selectedGym={selectedGym}
